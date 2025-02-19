0f30d888fc32d4527ceb1833bd8d3fbc6ed3bace
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2025-02-19 08:58:43 -0500

    Simplified simulation

--- Simulations/simulate_phase_history.m
+++ Simulations/simulate_phase_history.m
@@ -1,8 +1,8 @@
 % [samples_tT, t_fast, T_slow] = simulate_phase_history(radar, aperture, targets)
 %   Inputs are structures,
-%   radar: lambda, tau, B, f_s, phase
-%   aperture: F_prf, X_aT, V_aT, look_aaT, t_min, t_max, G_ant(sin_az, sin_el)
-%   targets: X_aN, sig_1N
+%   radar: wavelength, tau, B, sample_freq, phase
+%   aperture: F_prf, platform_posn_3T, V_aT, platform_look_33T, t_range(1), t_max, G_ant(sin_az, sin_el)
+%   targets: plat_posn_3N, sig_1N
 %   
 %   samples_tT:     phase history
 %   t_fast:         fast-time vector
@@ -12,71 +12,72 @@
 % target RCS. Transmit power, system losses, and noise must be included
 % separately.
 
-function [samples_tT, t_fast, T_slow, aux] = simulate_phase_history(radar, aperture, targets)
+function [samples, t_fast, T_slow] = simulate_phase_history(radar, aperture, targets)
     c = 299792458;
-    range2time = @(r) 2/c*r;
 
-    % Calculate geometry
-    x_tgt_a1N = permute(targets.X_aN, [1 3 2]); % permute for broadcasting
-    sig_tgt_11N = permute(targets.sig_N(:), [2 3 1]); % permute for broadcasting
-    r_tgt_aTN = aperture.X_aT - x_tgt_a1N; % displacement platform-to-target
-    R_tgt_1TN = vecnorm(r_tgt_aTN, 2, 1); % ranges 
-    u_tgt_aTN = r_tgt_aTN ./ R_tgt_1TN; % unit vectors
+    mustBeFields(radar, ["pulse", "sample_freq", "wavelength"])
+    mustBeFields(aperture, ["platform_position", "platform_velocity", ...
+        "platform_look", "t_range", "T_range"]);
+    mustBeFields(targets, ["target_position", "target_RCS"]);
 
-    t_tx_t = (0:(1/radar.f_s):radar.tau)' - radar.tau/2;
-    t_tgt_1TN = range2time(R_tgt_1TN);
-
-    % sample-justify received signal timing
-    t_samp_tTN = floor(t_tgt_1TN * radar.f_s) / radar.f_s + t_tx_t - t_tgt_1TN;
-    % calculate received signal destination indicies
-    i_tgt_1TN = floor(radar.f_s * (t_tgt_1TN - aperture.t_min - radar.tau/2));
-    i_tgt_eTN = cat(1, i_tgt_1TN, i_tgt_1TN + length(t_tx_t) - 1);
-    % max index to expand received sample array (if necessary) for targets at extreme corners
-    i_rx_max = max(i_tgt_eTN(2, :, [1 end]), [], "all");
-    t_max = max(aperture.t_max, i_rx_max/radar.f_s + aperture.t_min);
-
-    t_fast = (aperture.t_min:(1/radar.f_s):t_max)';
+    t_fast = aperture.t_range(1):(1/radar.sample_freq):aperture.t_range(2);
+    t_fast = t_fast';
     n_fast = length(t_fast);
 
-    N_slow = size(aperture.X_aT, 2);
-    T_slow = ((1:N_slow)-1)/aperture.F_prf;  
-    T_slow = T_slow - mean(T_slow); % 0-centered
+    T_slow = linspace(aperture.T_range(1), aperture.T_range(2), ...
+        size(aperture.platform_position, 2));
+    N_slow = length(T_slow);
 
-    % Doppler to each target
+    N_tgt = length(targets.target_RCS);
 
-    V_aTN = repmat(aperture.V_aT, 1, size(u_tgt_aTN, 2), size(u_tgt_aTN, 3));
-    F_dop_1TN = 2/radar.lambda * dot(V_aTN, u_tgt_aTN, 1); 
-    % Unit vector in antenna axes (left-x, forward-y, up-z)
-    u_tgt_ant_aTN = pagemtimes(aperture.look_aaT, u_tgt_aTN);
-    G_ant_1TN = aperture.G_ant(u_tgt_ant_aTN(1, :, :), u_tgt_ant_aTN(3, :, :));
-    h_rx_1TN = sqrt(sig_tgt_11N * (radar.lambda^2/(4*pi)^3) .* G_ant_1TN.^2 ./ R_tgt_1TN.^4);
-    K_r = radar.B / radar.tau;
-    s_rx_tTN = h_rx_1TN .* exp(1j*(-(4*pi/radar.lambda)*R_tgt_1TN + ...
-        pi*K_r * t_samp_tTN.^2 + 2*pi * F_dop_1TN.*t_samp_tTN));
+    samples = zeros(n_fast, N_slow);
 
-    samples_tT = zeros(n_fast, N_slow);
-    wb = waitbar(0, "Composing phase history");
-    N_pulses = size(s_rx_tTN, 2);
-    for slow_i = 1:N_pulses
+    % much or all of this can be vectorized, but the cost to readability is not
+    % worth it in my opinion
+    wb = waitbar(0, "Generating raw phase history", Name = "Progress");
+    oc = onCleanup(@() close(wb));
+    every = floor(N_slow/20);
 
-        if mod(slow_i, floor(N_pulses/10)) == 0
-            waitbar(slow_i/size(s_rx_tTN, 2), wb, sprintf("Pulse %d", slow_i));
+
+    for i_slow = 1:N_slow
+        for i_tgt = 1:N_tgt
+            r_tgt = targets.target_position(:, i_tgt) - aperture.platform_position(:, i_slow);
+            v_tgt = aperture.platform_velocity(:, i_slow);
+
+            R_tgt = norm(r_tgt);
+            t_tgt = 2*R_tgt/c;
+            u_tgt = r_tgt / R_tgt;
+            az = sin(u_tgt(1));
+            el = sin(u_tgt(1));
+
+            F_dop = 2/radar.wavelength * v_tgt' * u_tgt;
+            G_tx = aperture.tx_antenna_gain(az, el);
+            G_rx = aperture.rx_antenna_gain(az, el);
+            A_rx = sqrt(targets.target_RCS(i_tgt) * radar.wavelength^2/(4*pi)^3 * G_tx * G_rx / R_tgt^4);
+
+            phase_dop = 2*pi*F_dop*t_fast;
+            phase_tgt = -(4*pi/radar.wavelength)*R_tgt;
+            pulse = interp1(radar.pulse, (t_fast - t_tgt)*radar.sample_freq + 1, "linear", 0);
+            pulse(isnan(pulse)) = 0;
+            s_rx = A_rx .* exp(1j*(phase_dop + phase_tgt)) .* pulse;
+            
+            samples(:, i_slow) = samples(:, i_slow) + s_rx;
         end
 
-        for tar_i = 1:size(s_rx_tTN, 3)
-            slc = i_tgt_eTN(1, slow_i, tar_i):i_tgt_eTN(2, slow_i, tar_i);
-            samples_tT(slc, slow_i) = samples_tT(slc, slow_i) + s_rx_tTN(:, slow_i, tar_i);
+        if mod(i_slow, every) == 0
+            waitbar(i_slow / N_slow, wb, sprintf("Position %d of %d", i_slow, N_slow));
         end
     end
-    waitbar(1, wb, "Done.");
-    close(wb);
-    
-    % Cut off samples beyond max time
-    i_aperture = t_fast < aperture.t_max;
-    t_fast = t_fast(i_aperture);
-    samples_tT = samples_tT(i_aperture, :);
+    delete(oc);
+end
 
-    aux.R_tgt = squeeze(R_tgt_1TN);
-    aux.F_dop = squeeze(F_dop_1TN);
-    aux.G_ant = squeeze(G_ant_1TN);
+function mustBeFields(structure, fields)
+    fn = string(fieldnames(structure));
+    notpresent = setdiff(fields, fn);
+    name = inputname(1);
+    if ~isempty(notpresent)
+        mex = MException("simulate_phase_history:notfields", ...
+            "Required fields of %s not present: %s", name, mat2str(notpresent));
+        throwAsCaller(mex);
+    end
 end
