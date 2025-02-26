7dd5de96ffe6a5231c2a65984ed99dc87b8f8518
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2025-02-26 11:34:31 -0500

    Reorganized

--- Simulations/idealLFM.m
+++ Simulations/idealLFM.m
@@ -1,4 +1,4 @@
-function [pulse] = idealLFM(chirp_bandwidth, pulse_width, sample_freq)
+function [pulse, time] = idealLFM(chirp_bandwidth, pulse_width, sample_freq)
     % Generates an ideal centered LFM waveform at baseband
     % [pulse, f_s] = idealLFM(chirp_bandwidth, pulse_width)
     % INPUTS
@@ -7,9 +7,9 @@ function [pulse] = idealLFM(chirp_bandwidth, pulse_width, sample_freq)
     %   sample_freq         Frequency to sample signal
     % OUTPUTS
     %   pulse               baseband chirped signal samples (complex)
-    t = (0:(1/sample_freq):pulse_width)';
+    time = (0:(1/sample_freq):pulse_width)';
     chirp_rate = chirp_bandwidth/pulse_width;
-    pulse = exp(1j*(pi * chirp_rate .* (t - pulse_width/2).^2));
+    pulse = exp(1j*(pi * chirp_rate .* (time - pulse_width/2).^2));
     pulse = real(pulse);
 
     assert(iscolumn(pulse));
--- Utilities/vswr2gamma.m
+++
@@ -1,3 +0,0 @@
-function gamma = vswr2gamma(vswr)
-    gamma = (vswr-1)./(vswr+1);
-end
